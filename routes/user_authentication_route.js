bcdac3ee77a888c80de7549e02ae078166a36b84
Rukyy <rukemeniemogha38@gmail.com>
2024-01-05 23:40:53 +0100

    change signup route

--- routes/user_authentication_route.js
+++ routes/user_authentication_route.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const {createUser, LoginUser, logOut, authStatus,getUserDetails} = require("../controllers/user_authentication")
 
 
-router.post("/", createUser)
+router.post("/signup", createUser)
 router.post("/signin",LoginUser)
 router.get("/", logOut)
 router.get("/", authStatus)
